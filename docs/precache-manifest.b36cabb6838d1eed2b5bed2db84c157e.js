8be7e3b9eeb0e5ad2205fea2fd20dd5d2ea1b2c4
toutpt <toutpt@gmail.com>
2020-07-10 17:03:36 +0200

    doc: update

---
+++ docs/precache-manifest.b36cabb6838d1eed2b5bed2db84c157e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed6bd01cc5ac104e6b1165ad364b88a",
+    "url": "/poc-react-xstate/index.html"
+  },
+  {
+    "revision": "0a82c4093c1c3decea8c",
+    "url": "/poc-react-xstate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5789432615d2eb790ee",
+    "url": "/poc-react-xstate/static/js/2.35f822c4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/poc-react-xstate/static/js/2.35f822c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a82c4093c1c3decea8c",
+    "url": "/poc-react-xstate/static/js/main.3d1a12dc.chunk.js"
+  },
+  {
+    "revision": "8419acc2ece7927902ee",
+    "url": "/poc-react-xstate/static/js/runtime-main.0a5393f0.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48f52a7812ebeb336c8809e9b1298f27.js"
+  "/poc-react-xstate/precache-manifest.b36cabb6838d1eed2b5bed2db84c157e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poc-react-xstate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
